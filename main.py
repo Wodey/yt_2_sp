7c3522a3dc49e5883694490d62bc3f9b7dd4915d
Wodey <54207104+Wodey@users.noreply.github.com>
2021-12-03 23:39:06 +0300

    now search song function works better!

--- main.py
+++ main.py
@@ -13,21 +13,19 @@ sp = spotipy.Spotify(auth_manager=auth_manager)
 
 #([a-z'=:\d]\s*)+ - ([a-z=:\d']\s*)+ - regular expression for this(may has some problems with matches groups
 
-def search_a_song(song, artist):
+def search_a_song(song):
     ruth_res = sp.search(song, type="track")
     if len(res := ruth_res['tracks']['items']) < 1:
         print("Can't find a track")
         return
 
-    for i in res:
-        if i['artists'][0]['name'] == artist:
-            print(i['uri'])
-            return i['uri']
-    print("Can't find a track")
+    res = res[0]
+
+    print(res['uri'])
+    return res['uri']
 
 for i in songs:
     res = re.search("([a-z'=:\d]\s*)+ - ([a-z=:\d']\s*)+", i, flags=re.IGNORECASE).group(0)
-    artist, song = res.split(' - ')
-    search_a_song(song,artist)
+    search_a_song(res)
 
 
