4ba644d43142e2e660f83edadb5f1d8ef4c28273
Wodey <54207104+Wodey@users.noreply.github.com>
2021-12-03 23:29:19 +0300

    it searches for songs in the file and print the urls of them, but it makes some mistakes

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
 import re
+import spotipy
+import os
+from dotenv import load_dotenv
+from spotipy.oauth2 import SpotifyClientCredentials
+
+load_dotenv()
 
 songs = open('songs.txt', 'r')
 
+auth_manager = SpotifyClientCredentials()
+sp = spotipy.Spotify(auth_manager=auth_manager)
+
 #([a-z'=:\d]\s*)+ - ([a-z=:\d']\s*)+ - regular expression for this(may has some problems with matches groups
 
+def search_a_song(song, artist):
+    ruth_res = sp.search(song, type="track")
+    if len(res := ruth_res['tracks']['items']) < 1:
+        print("Can't find a track")
+        return
+
+    for i in res:
+        if i['artists'][0]['name'] == artist:
+            print(i['uri'])
+            return i['uri']
+    print("Can't find a track")
+
 for i in songs:
-    res = re.search("([a-z'=:\d]\s*)+ - ([a-z=:\d']\s*)+", i, flags=re.IGNORECASE)
-    print(res.group(0))
+    res = re.search("([a-z'=:\d]\s*)+ - ([a-z=:\d']\s*)+", i, flags=re.IGNORECASE).group(0)
+    artist, song = res.split(' - ')
+    search_a_song(song,artist)
+
 
