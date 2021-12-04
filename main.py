2cb1ec2b3c2aaf2cc653e42c7d91a9cf1714867f
Wodey <54207104+Wodey@users.noreply.github.com>
2021-12-04 11:16:06 +0300

    Working script, it create playlist from songs.txt

--- main.py
+++ main.py
@@ -2,30 +2,44 @@ import re
 import spotipy
 import os
 from dotenv import load_dotenv
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 
 load_dotenv()
 
+#Open the file with the songs list
 songs = open('songs.txt', 'r')
 
-auth_manager = SpotifyClientCredentials()
-sp = spotipy.Spotify(auth_manager=auth_manager)
+scope = "playlist-modify-public"
 
-#([a-z'=:\d]\s*)+ - ([a-z=:\d']\s*)+ - regular expression for this(may has some problems with matches groups
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
+#([a-z'=:\-\d]\s*)+ - ([a-z=:\-\d']\s*)+ - regular expression for this task
+
+name = input("Input a name of the playlist: ")
+description = input("Input a description of the playlist: ")
+
+#function that search for a song with spotify api, it returns songs url if it found it else it returns none
 def search_a_song(song):
     ruth_res = sp.search(song, type="track")
     if len(res := ruth_res['tracks']['items']) < 1:
-        print("Can't find a track")
         return
 
     res = res[0]
-
     print(res['uri'])
     return res['uri']
 
+#list of all songs that must be in the playlist
+songs_for_playlist = []
+
+# iteration over the songs list and add each song to songs_for_playlist varialble
 for i in songs:
     res = re.search("([a-z'=:\d]\s*)+ - ([a-z=:\d']\s*)+", i, flags=re.IGNORECASE).group(0)
-    search_a_song(res)
+    songs_for_playlist.append(search_a_song(res))
 
+#it creates a playlist
+playlist_id = sp.user_playlist_create(os.getenv("USER_NAME"), name, description=description)['id']
 
+# delete all none values from songs_for_playlist
+songs_for_playlist = [x for x in songs_for_playlist if x is not None]
+
+sp.playlist_add_items(playlist_id, songs_for_playlist)
